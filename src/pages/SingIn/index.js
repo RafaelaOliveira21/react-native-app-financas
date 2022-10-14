f8dd07183073309727b97ef3692c35d635b0ccb6
contabilizai <>
2022-10-14 12:51:08 -0300

    Update 14-10

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,10 +1,69 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import * as Animatable from "react-native-animatable";
+import { useNavigation } from "@react-navigation/native";
 
 export default function SingIn() {
   return (
-    <View>
-      <Text>Tela Login</Text>
+    <View style={styles.container}>
+      <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
+        <Text style={styles.message}>Bem-Vindo(a)</Text>
+      </Animatable.View>
+
+      <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+        <Text style={styles.title}>Email</Text>
+        <TextInput
+          placeholder="Digite seu email"
+          style={styles.input}
+        />
+
+        <Text style={styles.title}>Senha</Text>
+        <TextInput
+          placeholder="Digite sua senha"
+          style={styles.input}
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonRegister}>
+          <Text style={styles.registerText}>Não possui uma conta? Cadastre-se</Text>
+        </TouchableOpacity>
+      </Animatable.View>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0080E0',
+  },
+  containerHeader: {
+    marginTop: '14%',
+    marginBottom: '8%',
+    paddingStart: '5%',
+  },
+  message: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  containerForm: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: '5%',
+    paddingEnd: '5%',
+  },
+  title: {
+    fontSize: 20,
+    marginTop: 28,
+  },
+  input: {},
+
+
+});
--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,23 +1,31 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import * as Animatable from "react-native-animatable";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Welcome() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <View style={styles.containerLogo}>
-        <Image source={require("../../../assets/Fundo.png")}
+        <Animatable.Image
+          animation="flipInY"
+          source={require("../../../assets/Fundo.png")}
           style={{ width: '100%', height: 300 }}
           resizeMode="contain" />
       </View>
 
-      <View style={styles.containerForm}>
+      <Animatable.View delay={600} animation="fadeInUp" style={styles.containerForm}>
         <Text style={styles.title}>Monitore, organize seus gastos de qualquer lugar!</Text>
         <Text style={styles.text}>Faça o login para começar</Text>
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('SingIn')}>
           <Text style={styles.buttonText}>Acessar</Text>
         </TouchableOpacity>
-      </View>  
+      </Animatable.View>  
     </View>
   );
 }
@@ -46,5 +54,24 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginTop: 28,
     marginBottom: 12,
+  },
+  text: {
+    color: '#A1A1A1',
+  },
+  button: {
+    position: 'absolute',
+    backgroundColor: '#0080E0',
+    borderRadius: 50,
+    paddingVertical: 8,
+    width: '60%',
+    alignSelf: 'center',
+    bottom: '15%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
   },
 });
