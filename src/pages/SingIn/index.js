b92c8a7b46d35a15392994c4ff60dc37e2d42b4c
contabilizai <>
2022-10-17 10:58:37 -0300

    Finish

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -63,7 +63,31 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginTop: 28,
   },
-  input: {},
-
-
+  input: {
+    borderBottomWidth: 1,
+    height: 40,
+    marginBottom: 12,
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#0080E0',
+    width: '100%',
+    borderRadius: 17,
+    paddingVertical: 8,
+    marginTop: 14,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  buttonRegister: {
+    marginTop: 14,
+    alignSelf: 'center',
+  },
+  registerText: {
+    color: '#a1a1a1',
+  },  
 });
